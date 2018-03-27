def3ed925c104ca815ce872200d960f6d7fae1de
zzzadruga <zzzadruga@gmail.com>
2018-03-27 15:43:35 +0300

    homework 2 version 2

--- src/main/java/ru/sbt/jschool/session2/OutputFormatter.java
+++ src/main/java/ru/sbt/jschool/session2/OutputFormatter.java
@@ -23,6 +23,7 @@ import java.io.PrintStream;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 /**
@@ -33,13 +34,15 @@ public class OutputFormatter {
     private DecimalFormatSymbols formatSymbols = new DecimalFormatSymbols();
     private DecimalFormat moneyFormat = createFormat(new DecimalFormat("###,##0.00"), formatSymbols);
     private DecimalFormat numberFormat = createFormat(new DecimalFormat(), formatSymbols);
-    private boolean[] isString;
+    private String[] types;
 
     public OutputFormatter(PrintStream out) {
         this.out = out;
     }
 
     public void output(String[] names, Object[][] data) {
+        types = new String[names.length];
+        Arrays.fill(types, "");
         Integer[] maxLength = getMaxLength(names, data);
         String horizontalTableBorder = createHorizontalTableBorder(maxLength);
         this.out.println(horizontalTableBorder);
@@ -63,33 +66,49 @@ public class OutputFormatter {
         String obj;
         for (int i = 0; i < objects.length; i++) {
             this.out.printf(
-                    (i == 0 ? "|" : "") + "%" + ((isString[i] && !names) ? "-" : "") +
+                    (i == 0 ? "|" : "") + "%" + ((types[i].equals("String") && !names) ? "-" : "") +
                             maxLength[i] + "s|" + (i == objects.length - 1 ? "%n" : ""),
                     names ? (obj = objects[i].toString())
                             .concat(StringUtils.repeat(' ', (maxLength[i] - obj.length()) / 2 + (maxLength[i] - obj.length()) % 2))
-                          : getValue(objects[i]));
+                          : getValue(i, objects[i]));
         }
     }
 
     private Integer[] getMaxLength(String[] names, Object[][] data) {
         Integer[] maxLength = new Integer[names.length];
-        isString = new boolean[names.length];
         for (int i = 0; i < names.length; i++) {
             maxLength[i] = names[i] == null ? 1 : names[i].length();
         }
         for (int i = 0; i < data.length; i++) {
             int length;
             for (int j = 0; j < data[i].length; j++) {
-                length = data[i][j] == null ? 1 : getValue(data[i][j]).length();
-                if (!isString[j]) {
-                    isString[j] = data[i][j] instanceof String;
+                if (types[j].isEmpty()) {
+                    types[j] = getTypeName(data[i][j]);
                 }
+                length = data[i][j] == null ? 1 : getValue(j, data[i][j]).length();
                 if (length > maxLength[j]) {
                     maxLength[j] = length;
                 }
             }
         }
         return maxLength;
+    }
+
+    private String getTypeName(Object object){
+        if (object instanceof Date) {
+            return "Date";
+        }
+        if (object instanceof Number) {
+            if (object instanceof Float || object instanceof Double) {
+                return "Money";
+            } else {
+                return "Number";
+            }
+        }
+        if (object instanceof String) {
+            return "String";
+        }
+        return null;
     }
 
     private DecimalFormat createFormat(DecimalFormat format, DecimalFormatSymbols symbols) {
@@ -101,20 +120,15 @@ public class OutputFormatter {
         return format;
     }
 
-    private String getValue(Object object) {
+    private String getValue(int i, Object object) {
         if (object == null) {
             return "-";
         }
-        if (object instanceof Date) {
-            return dateFormat.format(object);
+        switch(types[i]){
+            case "Date" : return dateFormat.format(object);
+            case "Money" : return moneyFormat.format(object);
+            case "Number" : return numberFormat.format(object);
+            default: return object.toString();
         }
-        if (object instanceof Number) {
-            if (object instanceof Float || object instanceof Double) {
-                return moneyFormat.format(object);
-            } else {
-                return numberFormat.format(object);
-            }
-        }
-        return object.toString();
     }
 }
